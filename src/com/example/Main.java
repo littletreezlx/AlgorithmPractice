4b8307b392469dcea94c07a37f23460feea3f225
vincent <602788458@qq.com>
2019-05-23 17:23:12 +0800

    5.23

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,23 +3,15 @@ package com.example;
 import com.example.linkedlist.MyStack;
 import com.example.linkedlist.Solution;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-//        MyStack myStack = new MyStack();
-//        myStack.push(1);
-//        myStack.push(2);
-//
-//        System.out.println(myStack.top());
-//        myStack.push(3);
-//
-//        System.out.println(myStack.top());
-
-
-        Solution s = new Solution();
-        System.out.println(s.isValid("["));
-
+        int totalReward = 10;
+        Lesson5_1.get(totalReward, new ArrayList<>());
 
     }
+
 }
---
+++ src/com/example/T239.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class T239 {
+
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+
+
+            Deque<Integer> deque = new ArrayDeque<>();
+
+        }
+    }
+
+
+    class
+
+
+
+
+
+}
